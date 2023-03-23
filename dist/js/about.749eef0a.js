9d196201b31ef755ca4afc3d86339bdab121a22e
Sebastien Goldberg <sebastien.goldberg@gmail.com>
2023-03-23 19:02:04 +0100

    New Deployment

--- dist/js/about.749eef0a.js
+++ dist/js/about.749eef0a.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkfiduciaire_monaux"]=self["webpackChunkfiduciaire_monaux"]||[]).push([[443],{7381:function(n,u,a){a.r(u),a.d(u,{default:function(){return d}});var i=a(3396);const e={class:"about"},r=(0,i._)("h1",null,"This is an about page",-1),t=[r];function c(n,u){return(0,i.wg)(),(0,i.iD)("div",e,t)}var s=a(89);const o={},f=(0,s.Z)(o,[["render",c]]);var d=f}}]);
-//# sourceMappingURL=about.42728178.js.map
+//# sourceMappingURL=about.749eef0a.js.map
