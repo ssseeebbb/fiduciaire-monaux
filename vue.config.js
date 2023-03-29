3788b20dae85bf662c9bb5a9af7a71e58e1695be
Sebastien Goldberg <sebastien.goldberg@gmail.com>
2023-03-29 13:07:10 +0200

    ajout

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/fiduciaire-monaux/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/fiduciaire-monaux/" : "/",
   transpileDependencies: true
 })
