bb054b7c484c4262cee7ff9efc57729d63aa05cc
Sebastien Goldberg <sebastien.goldberg@gmail.com>
2023-03-29 13:16:36 +0200

    remove google source map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -14,211 +14,211 @@
 </template>
 
 <script setup>
-function initMap() {
-  // The location of Fiduciaire
-  const monaux = { lat: 50.357139, lng: 4.433039 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: monaux,
-    styles: [
-      {
-        featureType: "administrative",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#444444",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "geometry.stroke",
-        stylers: [
-          {
-            weight: "0.40",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "labels.text",
-        stylers: [
-          {
-            visibility: "on",
-          },
-          {
-            saturation: "-40",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#333366",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.country",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            hue: "#ff0000",
-          },
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.province",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.locality",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.neighborhood",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "administrative.land_parcel",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "landscape",
-        elementType: "all",
-        stylers: [
-          {
-            color: "#ffffff",
-          },
-        ],
-      },
-      {
-        featureType: "landscape.man_made",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "landscape.natural",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "poi",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "road",
-        elementType: "all",
-        stylers: [
-          {
-            saturation: -100,
-          },
-          {
-            lightness: 45,
-          },
-          // {
-          //     "weight": "1.25"
-          // },
-          {
-            visibility: "on",
-          },
-        ],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "simplified",
-          },
-        ],
-      },
-      {
-        featureType: "road.arterial",
-        elementType: "labels.icon",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "transit",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "off",
-          },
-        ],
-      },
-      {
-        featureType: "water",
-        elementType: "all",
-        stylers: [
-          {
-            visibility: "on",
-          },
-          {
-            color: "#D5E0EC",
-          },
-        ],
-      },
-    ],
-  });
-  // The marker, positioned at monaux
-  const marker = new google.maps.Marker({
-    position: monaux,
-    icon: "/files/marker.png",
-    map: map,
-  });
-}
+// const initMap = () => {
+//   // The location of Fiduciaire
+//   const monaux = { lat: 50.357139, lng: 4.433039 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 13,
+//     center: monaux,
+//     styles: [
+//       {
+//         featureType: "administrative",
+//         elementType: "labels.text.fill",
+//         stylers: [
+//           {
+//             color: "#444444",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.country",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "on",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.country",
+//         elementType: "geometry.stroke",
+//         stylers: [
+//           {
+//             weight: "0.40",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.country",
+//         elementType: "labels.text",
+//         stylers: [
+//           {
+//             visibility: "on",
+//           },
+//           {
+//             saturation: "-40",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.country",
+//         elementType: "labels.text.fill",
+//         stylers: [
+//           {
+//             color: "#333366",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.country",
+//         elementType: "labels.text.stroke",
+//         stylers: [
+//           {
+//             hue: "#ff0000",
+//           },
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.province",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.locality",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "on",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.neighborhood",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "administrative.land_parcel",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "landscape",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             color: "#ffffff",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "landscape.man_made",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "landscape.natural",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "on",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "poi",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "road",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             saturation: -100,
+//           },
+//           {
+//             lightness: 45,
+//           },
+//           // {
+//           //     "weight": "1.25"
+//           // },
+//           {
+//             visibility: "on",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "simplified",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "road.arterial",
+//         elementType: "labels.icon",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "transit",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "off",
+//           },
+//         ],
+//       },
+//       {
+//         featureType: "water",
+//         elementType: "all",
+//         stylers: [
+//           {
+//             visibility: "on",
+//           },
+//           {
+//             color: "#D5E0EC",
+//           },
+//         ],
+//       },
+//     ],
+//   });
+//   // The marker, positioned at monaux
+//   const marker = new google.maps.Marker({
+//     position: monaux,
+//     icon: "/files/marker.png",
+//     map: map,
+//   });
+// }
 
 // window.initMap = initMap;
 </script>
